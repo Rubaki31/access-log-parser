5820da28ba3a93710eca380a93f360132fa541d3
Rishat Minnigaliev <lokkol89@gmail.com>
2025-05-31 02:25:33 +0500

    Курсовой проект. Исправлено

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int counter = 1;
+        int counter = 0;
+        System.out.println("Введите адрес файла");
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExist = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (fileExist == false || isDirectory == false) {
-                if (fileExist == false) {
-                    System.out.println("файл не существует");
-                } else {
-                    System.out.println("указанный путь является путём к папке");
-                }
+            if (!fileExist){
+                System.out.println("указанный файл не существует");
+                continue;
+            }
+            if(isDirectory){
+                System.out.println("указанный путь является путём к папке");
                 continue;
             }
             counter++;
-            System.out.println("Путь указан верно Это файл номер" + counter);
+
+            System.out.println("Путь указан верно Это файл номер " + counter);
         }
     }
 }
