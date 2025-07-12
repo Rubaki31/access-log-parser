b1a46ddc333262fe41871f4bf0bc9061ba424de4
Rishat Minnigaliev <lokkol89@gmail.com>
2025-07-12 12:35:06 +0300

    Курсовой проект. Задание #1 по теме "Collections"

--- src/alp/Statistics.java
+++ src/alp/Statistics.java
@@ -1,4 +1,5 @@
 package alp;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -9,8 +10,10 @@ public class Statistics {
     private long totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
-    private final HashSet <String> sites = new HashSet<>();
-    private final HashMap<String,Integer> oses = new HashMap<>();
+    private final HashSet<String> sites = new HashSet<>();
+    private final HashMap<String, Integer> oses = new HashMap<>();
+    private final HashSet<String> notSites = new HashSet<>();
+    private final HashMap<String, Integer> browsers = new HashMap<>();
 
     public Statistics() {
         this.totalTraffic = 0;
@@ -22,44 +25,69 @@ public class Statistics {
     public void addEntry(LogEntry entry) {
         this.totalTraffic += Integer.parseInt(entry.value);
         LocalDateTime dateTrafic = entry.date;
-        if (dateTrafic.isBefore(minTime)){
-            minTime=dateTrafic;
+        if (dateTrafic.isBefore(minTime)) {
+            minTime = dateTrafic;
         }
-        if(dateTrafic.isAfter(maxTime)){
-            maxTime=dateTrafic;
+        if (dateTrafic.isAfter(maxTime)) {
+            maxTime = dateTrafic;
         }
-        if(entry.code==200){
+        if (entry.code == 200) {
             sites.add(String.valueOf(entry.method));
         }
-        UserAgent ua  = new UserAgent(entry.userAgent);
-        String os = ua.getOs();
-        if (oses.containsKey(os)){
-            oses.put(os,oses.get(os)+1);
-        } else {
-            oses.put(os,1);
+        if (entry.code == 404) {
+            notSites.add(String.valueOf(entry.method));
         }
+        UserAgent ua = new UserAgent(entry.userAgent);
+        String os = ua.getOs();
+        String browser = ua.getBrowser();
+        if (oses.containsKey(os)) {
+            oses.put(os, oses.get(os) + 1);
+        } else {
+            oses.put(os, 1);
+        }
+        if (browsers.containsKey(browser)) {
+            browsers.put(browser, browsers.get(browser) + 1);
+        } else {
+            browsers.put(browser, 1);
+        }
+
     }
-    public int getTrafficRate (){
+
+    public int getTrafficRate() {
         int rate = 0;
-        Duration duration = Duration.between(minTime,maxTime);
+        Duration duration = Duration.between(minTime, maxTime);
 
         long diffHours = duration.toHours();
         rate = Math.toIntExact(totalTraffic / diffHours);
         return rate;
     }
-    public HashMap<String,Double> osShares(){
+
+    public HashMap<String, Double> osShares() {
         int total = 0;
-        for (int count: oses.values()){
+        for (int count : oses.values()) {
             total += count;
         }
-        for (Map.Entry<String,Integer> entry: oses.entrySet()){
+        for (Map.Entry<String, Integer> entry : oses.entrySet()) {
             String os1 = entry.getKey();
             int count = entry.getValue();
-            double shares = (double) count/total;
-            osShares().put(os1,shares);
+            double shares = (double) count / total;
+            osShares().put(os1, shares);
         }
 
         return osShares();
+    }
+    public HashMap<String,Double>browserShares(){
+        int total = 0;
+        for (int count:browsers.values()){
+            total+=count;
+        }
+        for (Map.Entry<String,Integer>entry: browsers.entrySet()){
+            String br= entry.getKey();
+            int count = entry.getValue();
+            double shares= (double) count/total;
+            browserShares().put(br,shares);
+        }
+        return browserShares();
     }
 
     public HashSet<String> getSites() {
